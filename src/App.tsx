d56f5753f9ba41d73f6a4c2ac843aa825e06bad2
Kyle Huynh <kyle_huynh@hotmail.com>
2024-04-23 13:38:28 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="bg-red-700">
+        Hello world!
+      </div>
+    </>
+  )
+}
+
+export default App
